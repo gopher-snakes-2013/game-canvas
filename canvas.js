d1c0bc848d1ff04778192e72bf8f5b0720e9bfd4
Apprentice <apprentice@dbc-29.local>
2013-11-08 10:44:13 -0800

    Added turtle

--- canvas.js
+++ canvas.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
   var c_canvas = document.getElementById("c");
   var context = c_canvas.getContext("2d");
   var my_gradient=context.createLinearGradient(0,0,0,170);
-  my_gradient.addColorStop(0,"red");
-  my_gradient.addColorStop(1,"white");
+  var turtleX=50
+  var turtleY=50
+
 
   makeGrid = function(){
     for (var x = 0.5; x < 1000; x += 50) {
@@ -19,10 +20,17 @@ $(document).ready(function(){
     context.stroke();
   };
 
-  makeRectangle = function(){
+  makeTurtle = function(){
+    my_gradient.addColorStop(0,"red");
+    my_gradient.addColorStop(1,"white");
     context.fillStyle=my_gradient;
      // upper-left corner (50, 50), width (50), height (50).
-    context.fillRect(50, 50, 50, 50);
+    context.fillRect(turtleX, turtleY, 50, 50);
+  };
+
+  drawLine = function(){
+    context.moveTo
+
   };
 
   resetCanvas = function(){
@@ -30,7 +38,7 @@ $(document).ready(function(){
   };
 
   makeGrid();
-  makeRectangle();
+  makeTurtle();
 
   console.log("Hello")
 
