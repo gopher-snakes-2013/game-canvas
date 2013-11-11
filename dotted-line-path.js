30c5d846b86f56b29ef2d793f91f47c733d624ad
Robert Taraya <robert.taraya@gmail.com>
2013-11-11 12:48:49 -0800

    add path drawLine and prepareContext prototypes

--- dotted-line-path.js
+++ dotted-line-path.js
@@ -1,3 +1,17 @@
-var dottedLine = function (){
+var DottedLine = function (){
   this.canvas = document.getElementById('dotted-line-canvas')
+  this.lineColor = "blue"
+  this.context = this.prepareContext()
+}
+
+DottedLine.prototype.prepareContext = function(){
+  return this.canvas.getContext('2d')
+}
+
+DottedLine.prototype.drawLine = function(x,y){
+  this.context.moveTo(0,0)
+  this.context.lineTo(x,y)
+  this.context.translate(x,y)
+  this.context.strokeStyle = this.lineColor
+  this.context.stroke()
 }
