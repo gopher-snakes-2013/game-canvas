a0877829999f5e48db09c578f36c1de570e91636
Apprentice <apprentice@dbc-16.local>
2013-11-11 15:40:24 -0800

    Canvas can now dynamically resize without blurring issues

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ $(document).ready(function(){
   var applicationController = new ApplicationController()
   applicationController.initializeGame()
 
-  // var PathCanvas = document.getElementById('path-canvas')
-  // var SpriteCanvas = document.getElementById('sprite-canvas')
-  // container = $('.canvas-container')
+  var PathCanvas = document.getElementById('path-canvas')
+  var SpriteCanvas = document.getElementById('sprite-canvas')
+  container = $('.canvas-container')
 
-  // PathCanvas.height = container.height()
-  // PathCanvas.width = container.width()
-
-  // spriteCanvas.height = container.height()
-  // spriteCanvas.width = container.width()
+  PathCanvas.height = container.height()
+  PathCanvas.width = container.width()
+  SpriteCanvas.height = container.height()
+  SpriteCanvas.width = container.width()
 })
 
