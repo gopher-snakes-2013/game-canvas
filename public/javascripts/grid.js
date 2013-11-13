5bc5a429d0b44922ad5a6cc1889b49dca65f8a2d
Apprentice <apprentice@dbc-28.local>
2013-11-12 16:47:25 -0800

    fixed line draw length discrepency

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -2,6 +2,8 @@ var Grid = function(color){
   this.canvas = document.getElementById('grid-canvas')
   this.lineColor = color
   this.context = this.prepareContext()
+  this.width = this.gridWidth()
+  this.height = this.gridHeight()
 }
 
 Grid.prototype.prepareContext = function(){
@@ -22,6 +24,25 @@ Grid.prototype.rotate = function(degrees) {
   this.context.rotate(degrees*Math.PI/180.0)
 }
 
-Grid.prototype.makeGridLines = function(){
+Grid.prototype.gridWidth = function() {
+    return this.canvas.width / 64;
+  }
 
+ Grid.prototype.gridHeight = function() {
+    return this.canvas.height / 36;
+  }
+
+Grid.prototype.makeGridLines = function(){
+  this.context.beginPath();
+    for (var y=this.gridHeight(); y<this.canvas.height; y+=this.gridHeight()) {
+      this.context.moveTo(0, y);
+      this.context.lineTo(this.canvas.width, y);
+    }
+    for (var x=this.gridWidth(); x<this.canvas.width; x+=this.gridWidth()) {
+      this.context.moveTo(x, 0);
+      this.context.lineTo(x, this.canvas.height);
+    }
+    this.context.strokeStyle = "#ddd";
+    this.context.closePath();
+    this.context.stroke();
 }
